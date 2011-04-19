5b508618a8051ca9017192652fc1a1243b7a65c6
Leon Coto <leoncoto@gmail.com>
2011-04-19 11:12:29 +0100

    Working now: - Datastore population script - Main handler - Project scaffold

--- main.py
+++ main.py
@@ -1,14 +1,114 @@
 #!/usr/bin/env python
 import logging
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+from google.appengine.dist import use_library
+use_library('django', '1.2')
 
+from google.appengine.ext.webapp import template # keep this import first
+from django import forms
+
+from google.appengine.api import users
+from google.appengine.ext import db
 from google.appengine.ext import webapp
+from google.appengine.ext.db import djangoforms
 from google.appengine.ext.webapp import util
 
+def get_default_template_values(dest_url=None): #redirect to current page
+    """
+    Returns default values used across templates
+    """
+    values = {}
+    values['user'] = users.get_current_user()
+    values['login_url'] = users.create_login_url(dest_url or '/')
+    values['logout_url'] = users.create_logout_url(dest_url or '/')
+    
+    return values
 
+def serve_banner():
+    """
+    Serves a banner. Takes care of recording the banner impression and also 
+    of sending placeholder content when there are no more banners
+    """
+    pass
+
+def get_random_banner():
+    
+    """
+    Returns a random bannner from the banners that still have impressions 
+    remaining.
+    Returns None if there are no banners with impressions remaining
+    """
+    pass
+
+def handle_queue_empty():
+    """
+    Handles all operations to be performed when all the banners have run our of
+    impressions
+    """
+    pass
+
+def record_banner_impression(banner):
+    """
+    Records (actually subtracts) a banner impression
+    """
+    pass
+
+
+class Team(db.Model):
+    """
+    BanterTarget representation
+    """
+    name = db.StringProperty(required=True)
+    enabled = db.BooleanProperty(default=True)
+
+
+class Banner(db.Model):
+    """
+    Banner representation
+    """
+    impressions = db.IntegerProperty(required=True, default=2000)
+    copy = db.StringProperty(required=True)
+    author = db.UserProperty(required=True)
+    team = db.ReferenceProperty(Team, required=True, collection_name='banners')
+
+
+class BannerForm(forms.Form):
+    """
+    A Simple form to save a Banner
+    """
+    pass
+    
+    
 class MainHandler(webapp.RequestHandler):
     def get(self):
-        logging.info('Hello world!')
-        self.response.out.write('Hello world!')
+        self.render('index.html', {'create_banner_url' : '/create_banner'})
+    
+    def render(self, template_file, template_values):
+        """
+        Convenience method to render templates
+        """
+        template_values.update(get_default_template_values(self.request.path))
+        path = os.path.join(os.path.dirname(__file__), 'templates/%s' %template_file)
+        self.response.out.write(template.render(path, template_values))
+
+
+class CreateBannerHandler(MainHandler):
+    """Creates a new Banner model in the datastore"""
+    
+    def get(self):
+        data = {
+            'teams' : BanterTarget.all(),
+            'form_action' : '/save_banner',
+        }
+        
+        self.render('create_banner.html', data)
+
+
+class SaveBannerHandler(MainHandler):
+    """Saves a banner in the datastore"""
+    def post(self):
+        self.redirect('/')
 
 
 def main():
@@ -16,6 +116,8 @@ def main():
     
     paths = [
         ('/', MainHandler),
+        ('/create_banner', CreateBannerHandler),
+        ('/save_banner', SaveBannerHandler),
     ];
     
     application = webapp.WSGIApplication(paths, debug=True)
---
+++ premier_league.py
@@ -0,0 +1,32 @@
+from main import Team
+
+for team in Team.all():
+    team.delete()
+
+teams = [
+    'Arsenal',
+    'Aston Villa',
+    'Birmingham City',
+    'Blackburn Rovers',
+    'Blackpool',
+    'Bolton Wanderers',
+    'Chelsea',
+    'Everton',
+    'Fulham',
+    'Liverpool',
+    'Manchester City',
+    'Manchester United',
+    'Newcastle United',
+    'Stoke City',
+    'Sunderland',
+    'Tottenham Hotspur',
+    'West Bromwich Albion',
+    'West Ham United',
+    'Wigan Athletic',
+    'Wolverhampton Wanderers',
+]
+
+for team in teams:
+    t = Team(name=team)
+    t.put()
+    print 'Team with name \"%s\" created.' %t.name
